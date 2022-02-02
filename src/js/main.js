0d0bab241e71c8c5d5b16a705da8b89264e0a041
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2022-02-02 00:30:38 -0800

    Improve the styling

--- src/js/main.js
+++ src/js/main.js
@@ -12,27 +12,27 @@ closeMenu.forEach((element) => {
   });
 });
 
-// const hideElement = document.querySelectorAll('.hide-cards');
-// const hideBtn = document.querySelector('.hide-btn');
-// const showItems = document.querySelector('.show-items');
-// const icon = document.querySelector('.hide-btn i');
-// const caretDown = ('fa', 'fa-chevron-down');
-// const caretUp = ('fa', 'fa-chevron-up');
+const hideElement = document.querySelectorAll('.hide-cards');
+const hideBtn = document.querySelector('.hide-btn');
+const showItems = document.querySelector('.show-items');
+const icon = document.querySelector('.hide-btn i');
+const caretDown = ('fa', 'fa-chevron-down');
+const caretUp = ('fa', 'fa-chevron-up');
 
-// hideElement.forEach((item) => {
-//   hideBtn.addEventListener('click', () => {
-//     if (item.style.visibility === 'visible') {
-//       item.style.visibility = 'hidden';
-//       item.style.height = '0';
-//       showItems.textContent = 'MORE';
-//       icon.classList.remove(caretUp);
-//       icon.classList.add(caretDown);
-//     } else {
-//       item.style.visibility = 'visible';
-//       item.style.height = '100%';
-//       showItems.textContent = 'LESS';
-//       icon.classList.remove(caretDown);
-//       icon.classList.add(caretUp);
-//     }
-//   });
-// });
+hideElement.forEach((item) => {
+  hideBtn.addEventListener('click', () => {
+    if (item.style.visibility === 'visible') {
+      item.style.visibility = 'hidden';
+      item.style.height = '0';
+      showItems.textContent = 'MORE';
+      icon.classList.remove(caretUp);
+      icon.classList.add(caretDown);
+    } else {
+      item.style.visibility = 'visible';
+      item.style.height = '100%';
+      showItems.textContent = 'LESS';
+      icon.classList.remove(caretDown);
+      icon.classList.add(caretUp);
+    }
+  });
+});
--- src/js/speakers.js
+++ src/js/speakers.js
@@ -1,86 +1,135 @@
-const programs = [
-  {
-    icon: 'fa-graduation-cap',
-    title: 'Lecture',
-    text: 'Listen to the most renowned personalities in the universe give motivate the youth on how best to stay on top',
-  },
-  {
-    icon: 'fa-university',
-    title: 'Workshop',
-    text: 'Get opportunities to see other peoples marvelous creations whiles exhibiting your own skills to the world.',
-  },
-  {
-    icon: 'fa-clone',
-    title: 'Forum',
-    text: 'Enjoy as open-minded people from all walks of life deliberate and share their thoughts and opinions with experts.',
-  },
-  {
-    icon: 'fa-handshake',
-    title: 'Network',
-    text: 'The summit offers you the greatest opportunity to meet and network with the best minds of our generation. Looking for mentorship? Look no further',
-  },
-  {
-    icon: 'fa-coins',
-    title: 'Funding',
-    text: 'Do you have any project or ideas that you need money for funding? The summit will never disappoint you. We have thousands of investors attending the program each year',
-  },
-];
-
-const speakers = [
+const featuredSpeakers1  = [
   {
     id: 1,
     name: 'Jeremiah Owusu',
     description: 'Founder and CEO at BestJ Group of Companies',
     about: 'Coming from a pharmaceutical background, Jerry has created a niche for himself in the tech industry. He believes that with the right mindset and support system, everyone can achieve the optimum best',
-    imageUrl: '/src/images/Jerry-owusu.jpeg',
+    img1: '/src/images/speaker-background.jpg',
+    img2: '/src/images/Jerry-owusu.jpeg',
   },
   {
     id: 2,
     name: 'Esther Owusu-Ansah',
     description: 'Physician Assistant, Co-founder and CEO at BestJ Rentals',
     about: 'In her most recent role as Chief Executive Officer, BestJ Rentals, she led them to their best productive years ever.',
-    imageUrl: '/src/images/Esther-owusu.jpeg',
-  },
+    img1: '/src/images/speaker-background.jpg',
+    img2: '/src/images/Esther-owusu.jpeg',
+  }
+]
+  const featuredSpeakers2 = [
   {
-    id: 3,
+    id: 1,
     name: 'Patrick Awuah',
     description: 'Founder and President of Ashesi University',
     about: 'Patrick Awuah is the Founder and President of Ashesi University, a private, not-for-profit institution that has quickly gained a reputation for innovation and quality education in Ghana. ',
-    imageUrl: '/src/images/patrick-awuah.jpg',
+    img1: '/src/images/speaker-background.jpg',
+    img2: '/src/images/patrick-awuah.jpg',
   },
   {
-    id: 4,
+    id: 2,
     name: 'Williams Adu',
     description: 'Senior Software Engineer at Microsoft ',
     about: 'After completing a degree in Telecommunication at the Kwame Nkrumah University of Science and Technology(KNUST), Williams Adu has pushed himself to become one of the best engineers the world has ever known',
-    imageUrl: '/src/images/williams-adu.jpg',
-  },
+    img1: '/src/images/speaker-background.jpg',
+    img2: '/src/images/williams-adu.jpg',
+  }
+]
+
+const featuredSpeakers3 = [ 
   {
-    id: 5,
+    id: 1,
     name: 'John Agyekum Kuffour',
     description: 'Former President of the Republic of Ghana',
     about: 'Former president of Ghana, a renowned lawyer and a businessman.',
-    imageUrl: '/src/images/John_Kufuor.jpg',
+    img1: '/src/images/speaker-background.jpg',
+    img2: '/src/images/John_Kufuor.jpg',
   },
   {
-    id: 6,
+    id: 2,
     name: 'Peter Yefi',
     description: 'Chief Technical Officer at Complete Farmer',
     about: 'Hands-on, successful software engineer and data science enthusiast with great analytical skills and in-depth knowledge in Python, ETL and Java. +3 years experience in Ghana, Rwanda, Kenya and US',
-    imageUrl: '/src/images/peter-yefi.jpg',
-  },
+    img1: '/src/images/speaker-background.jpg',
+    img2: '/src/images/peter-yefi.jpg',
+  }
 ];
 
-const programCard = document.getElementById('program-cards');
 
-programs.forEach((program) => {
-  programCard.innerHTML += `
-  <div class="card-contain col-md col-lg col-xl card h-100% p-0 m-0">
-      <div class='program-text row col-md-12 col-lg-12 col-xl-12 card-body p-0 m-0 bkg align-items-center'>
-      <i class='fa ${program.icon} white img1'></i>   
-      <h3 class='orange col-3 lato my-3 text-center col-md-12 col-lg-12 col-xl-12m-0' >${program.title}</h3>
-      <p class='grey text-center lato col-7 col-md-12 col-lg-12 col-xl-12' >${program.text}</p>
-      </div>
+
+const speakers1 = document.querySelector('.columns-row1');
+
+for (let i = 0; i < featuredSpeakers1.length; i += 1) {
+  const featuredSpeaker1 = featuredSpeakers1[i];
+
+  speakers1.innerHTML += `
+        
+            
+  <div  id='${featuredSpeaker1.id}' class='col-md-6 col-lg-5 col-xl-5 mt-3'>
+  <div class="row align-items-start justify-content-start">
+    <div class="images  col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 ">
+         <img class="image1 img-fluid" src="${featuredSpeaker1.img1}" alt="" width=60% height=80%>
+        <img class="image2 " src="${featuredSpeaker1.img2}" alt='${featuredSpeaker1.name}' width=80% height=100%>
     </div>
-      `;
-});
+    <div class="description align-items-center justify-content-center col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 mt-2">
+        <h4>${featuredSpeaker1.name}</h4>
+        <h5 class='red fst-italic fw-bold'>${featuredSpeaker1.description}
+        </h5>
+        <span class='line2'></span>
+        <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker1.about}</h6>
+    </div>
+   </div>
+    </div>               
+</div>
+`;
+}
+
+const speakers2 = document.querySelector('.columns-row2');
+
+for (let i = 0; i < featuredSpeakers2.length; i += 1) {
+  const featuredSpeaker2 = featuredSpeakers2[i];
+
+  speakers2.innerHTML += `
+        
+  <div  id='${featuredSpeaker2.id}' class='col-md-6 col-lg-5 col-xl-5 mt-3 '>
+  <div class="row align-items-start justify-content-start">
+    <div class="images  col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+         <img class="image1 img-fluid" src="${featuredSpeaker2.img1}" alt="" width=60% height=80%>
+        <img class="image2 " src="${featuredSpeaker2.img2}" alt='${featuredSpeaker2.name}' width=80% height=100%>
+    </div>
+    <div class="description align-items-center justify-content-center col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 mt-2">
+        <h4>${featuredSpeaker2.name}</h4>
+        <h5 class='red fst-italic fw-bold'>${featuredSpeaker2.description}
+        </h5>
+        <span class='line2'></span>
+        <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker2.about}</h6>
+    </div>
+   </div>
+    </div>               
+</div>
+`;
+}
+
+const speakers3 = document.querySelector('.columns-row3');
+
+for (let i = 0; i < featuredSpeakers2.length; i += 1) {
+  const featuredSpeaker3 = featuredSpeakers3[i];
+
+  speakers3.innerHTML += `
+  <div  id='${featuredSpeaker3.id}' class='col-md-6 col-lg-5 col-xl-5 mt-3'>
+  <div class="row align-items-start justify-content-start">
+    <div class="images  col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+         <img class="image1 img-fluid" src="${featuredSpeaker3.img1}" alt="" width=60% height=80%>
+        <img class="image2 " src="${featuredSpeaker3.img2}" alt='${featuredSpeaker3.name}' width=80% height=100%>
+  </div>
+    <div class="description align-items-center justify-content-center col-8 col-sm-8 col-md-8 col-lg-8 col-xl-8 mt-2">
+        <h4>${featuredSpeaker3.name}</h4>
+        <h5 class='red fst-italic fw-bold'>${featuredSpeaker3.description}
+        </h5>
+        <span class='line2'></span>
+        <h6 class='lato greyer py-1 me-4 fw-bold overflow-wrap'>${featuredSpeaker3.about}</h6>
+    </div>
+   </div>
+    </div>               
+</div>
+`;
+}
